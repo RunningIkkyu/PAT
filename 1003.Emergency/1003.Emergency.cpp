38b1b3516e3561b74267ff808d66965a774f46b1
RunningIkkyu <389498623@qq.com>
2018-02-02 17:22:50 +0800

    update README

---
+++ 1003.Emergency/1003.Emergency.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    int n, m, c1, c2;
+    int team_number[510];
+    fill(team_number, team_number+510, 0);
+    return 0;
+}
