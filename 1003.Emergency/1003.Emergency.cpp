bae5079f2ab67927dfda58b5c0b483e79694e5d8
RunningIkkyu <389498623@qq.com>
2018-02-04 13:57:47 +0800

    update

--- 1003.Emergency/1003.Emergency.cpp
+++ 1003.Emergency/1003.Emergency.cpp
@@ -7,5 +7,7 @@ int main(void)
     int n, m, c1, c2;
     int team_number[510];
     fill(team_number, team_number+510, 0);
+    cin>>n>>m>>c1>>c2;
+    for(int i = 0; i < m; i++) cin>>team_number[i];
     return 0;
 }
