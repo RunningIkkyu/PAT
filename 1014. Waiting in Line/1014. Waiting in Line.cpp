9c01f96f490a16d05eb842c4b283e788e518ab6c
RunningIkkyu <389498623@qq.com>
2018-03-01 22:41:38 +0800

    update 1014.Waition in Line.cpp

--- 1014. Waiting in Line/1014. Waiting in Line.cpp
+++ 1014. Waiting in Line/1014. Waiting in Line.cpp
@@ -1,40 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <queue>
 using namespace std;
 
 struct node{
     int poptime, endtime;
     queue<int> q;
-}
+};
+
 int main(void){
     // variable defination.
     int n, m, k, q, index = 1;
     cin>>n>>m>>k>>q;
-    vector<int> time(k+1), result(k+1)
+    vector<int> time(k+1), result(k+1);
+   
     vector<node> window(n+1);
     vector<bool> sorry(k+1, false);
+
     // input
-    for(int i = 1; i <= k; i++) cin>>time[i];
-    for(int ind = 1; ind <= k; ind++) {
-        cin>>time[ind];
-        if(ind <= m*n){
-            // window number.
-            int i = (ind-1)%3+1;
-            // j means the j-th people in one line.
-            int j = (ind-1)/3;
-            window[i].q.push(time[ind]);
-            if(window[i].endtime > 540) sorry[index] = true;
-            window[i].endtime += time[i];
-            if(ind == 1)
-                window[i].poptime = window[i].endtime;
-            result[ind] = window[i].endtime;
-        }
-        else
-            index = ind;
-    }
-    /*
-    // put back all people with index not above n*m.
+     for(int i = 1; i <= k; i++) cin>>time[i];
+// put back all people with index not above n*m.
     for(int i = 1; i <= m; i++){
         for(int j = 1; j <= n; j++){
             if(index <= k){
@@ -53,18 +39,38 @@ int main(void){
             }//if
         }//for
     }//for
-    */
+   //aaaaaaaaaaaaaaa
 
     while(index <= k){
         int tempmin = window[1].poptime;
-        int tempwindow = -1;
+        int tempwindow = 1;
+        // find first pop queue.
         for(int i = 2; i <= n; i++){
             if(window[i].poptime < tempmin){
                 tempwindow = i;
                 tempmin = window[i].poptime;
             }
         }
+        window[tempwindow].q.pop();
+        window[tempwindow].q.push(time[index]);
+        window[tempwindow].poptime += window[tempwindow].q.front();
+        if(window[tempwindow].endtime >= 540)
+            sorry[index] = true;
+        window[tempwindow].endtime += time[index];
+        result[index] = window[tempwindow].endtime;
+        index++;
+    }
 
+    for(int i = 1; i <= q; i++){
+        int query, minute;
+        cin>>query;
+        minute = result[query];
+        if(sorry[query] == true) cout<<"Sorry"<<endl;
+        else {
+            cout<<setfill('0')<<setw(2)<<(minute + 480) / 60;
+            cout<<":";
+            cout<<setfill('0')<<setw(2)<<(minute + 480) % 60<<endl;
+        }
     }
 
     return 0;
