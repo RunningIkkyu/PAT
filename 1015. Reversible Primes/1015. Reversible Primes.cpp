4bcfe4cc8a7d2a8f0fa770fff83b49aa1179cffe
RunningIkkyu <389498623@qq.com>
2018-03-02 11:58:51 +0800

    add 1015.Reversible Prime.cpp

---
+++ 1015. Reversible Primes/1015. Reversible Primes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num){
+    int sqr = int(sqrt(num * 1.0));
+    for(int i = 2; i <= sqr; i++){
+        if(num % i == 0) return false;
+    }
+    if(num <= 1) return false;
+    return true;
+}
+
+// convert to the given base, reverse and return 10 base number.
+int reverse(int num, int base){
+    int sum = 0;
+    for(; num != 0; num /= base) sum = (sum*base) +num % base;
+    return sum;
+}
+
+int main(void){
+    int n, b;
+    while(cin>>n, n >= 0){
+        cin>>b;
+        if(isPrime(n) && isPrime(reverse(n, b))) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+    return 0;
+}
