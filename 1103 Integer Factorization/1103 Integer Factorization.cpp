dc5949e0fa6366362cace0cf1e99cb091e5d7f43
RunningIkkyu <389498623@qq.com>
2018-07-03 20:52:49 +0800

    ADD 1103

---
+++ 1103 Integer Factorization/1103 Integer Factorization.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int n, k, p, maxFacSum = -1;
+vector<int> v, ans, tempAns;
+void init() {
+    int temp = 0, index = 1;
+    while(temp <= n) {
+        v.push_back(temp);
+        temp = pow(index, p);
+        index++;
+    }
+}
+void dfs(int index, int tempSum, int tempK, int facSum) {
+    if(tempSum == n && tempK == k) {
+        if(facSum > maxFacSum) {
+            ans = tempAns;
+            maxFacSum = facSum;
+        }
+        return ;
+    }
+    if(tempSum > n || tempK > k) return ;
+    if(index >= 1) {
+        tempAns.push_back(index);
+        dfs(index, tempSum + v[index], tempK + 1, facSum + index);
+        tempAns.pop_back();
+        dfs(index - 1, tempSum, tempK, facSum);
+    }
+}
+int main() {
+    scanf("%d%d%d", &n, &k, &p);
+    init();
+    dfs(v.size() - 1, 0, 0, 0);
+    if(maxFacSum == -1) {
+        printf("Impossible");
+        return 0;
+    }
+    printf("%d = ", n);
+    for(int i = 0; i < ans.size(); i++) {
+        if(i != 0) printf(" + ");
+        printf("%d^%d", ans[i], p);
+    }
+    return 0;
+}
